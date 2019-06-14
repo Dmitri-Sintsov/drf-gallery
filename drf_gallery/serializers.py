5c7a2cc70f0c9fa4164d3db0e69fc72423ad1412
Dmitriy Sintsov <questpc256@gmail.com>
2019-06-14 18:32:15 +0300

    Basic version of SerializerSerializer.

--- drf_gallery/serializers.py
+++ drf_gallery/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from rest_framework import fields as drf_fields, serializers
 
 
 # https://medium.com/django-rest-framework/dealing-with-unique-constraints-in-nested-serializers-dade33b831d9
@@ -42,3 +42,67 @@ class DynamicFieldsModelSerializer(serializers.ModelSerializer):
             existing = set(self.fields)
             for field_name in existing - allowed:
                 self.fields.pop(field_name)
+
+
+class SerializerSerializer(serializers.BaseSerializer):
+
+    def __init__(self, serializer=None, data=drf_fields.empty, read_only=None, flat=False, **kwargs):
+        self.read_only = read_only
+        self.flat = flat
+        super().__init__(instance=serializer, data=data)
+
+    def field_to_struct(self, field):
+        typ = 'text'
+        if isinstance(field, drf_fields.DateField):
+            typ = 'date'
+        elif isinstance(field, drf_fields.DateTimeField):
+            typ = 'datetime'
+        ret = {
+            'label': field.label,
+            'type': typ,
+        }
+        return ret
+
+    # See representation.serializer_repr().
+    def serializer_to_struct(self, serializer, base_path='', force_many=None):
+        if force_many:
+            fields = force_many.fields
+        else:
+            fields = serializer.fields
+        ret = []
+        for field_name, field in fields.items():
+            if self.flat:
+                if base_path == '':
+                    nested_path = field_name
+                else:
+                    nested_path = base_path + '.' + field_name
+            else:
+                nested_path = ''
+            if hasattr(field, 'fields'):
+                field_data = self.serializer_to_struct(field, nested_path)
+            elif hasattr(field, 'child'):
+                child = field.child
+                if hasattr(child, 'fields'):
+                    field_data = self.serializer_to_struct(serializer, nested_path, force_many=child)
+                else:
+                    field_data = self.field_to_struct(field)
+            elif hasattr(field, 'child_relation'):
+                field_data = self.serializer_to_struct(field.child_relation, nested_path, force_many=field.child_relation)
+            else:
+                field_data = self.field_to_struct(field)
+            if self.flat:
+                if isinstance(field_data, list):
+                    ret.extend(field_data)
+                else:
+                    field_data['name'] = nested_path
+                    ret.append(field_data)
+            else:
+                if isinstance(field_data, list):
+                    field_data = {'relation': field_data}
+                field_data['name'] = field_name
+                ret.append(field_data)
+        return ret
+
+    def to_representation(self, serializer):
+        ret = self.serializer_to_struct(serializer)
+        return ret
