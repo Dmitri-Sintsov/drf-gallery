b35ae2cdc8be3fe6fb1a074b1df399a5dd2cf96a
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-10 18:02:29 +0300

    Adding testing rollup configurations.

---
+++ drf_gallery/static/components/rollup_debug.sh
@@ -0,0 +1,2 @@
+rm bundle.*
+rollup main.js --format es --sourcemap inline --name MainBundle --file bundle.js
---
+++ drf_gallery/static/components/rollup_production.sh
@@ -0,0 +1,2 @@
+rm bundle.*
+rollup main.js --format es --compact --sourcemap true --name MainBundle --file bundle.js
