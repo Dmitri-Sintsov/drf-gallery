ea28e914ee2a305dd8a0014493390b825a29698f
Dmitriy Sintsov <questpc256@gmail.com>
2021-03-24 11:28:58 +0300

    Django 3.2 compatibility.

--- app_gallery/apps.py
+++ app_gallery/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class AppGalleryConfig(AppConfig):
     name = 'app_gallery'
+    default_auto_field = 'django.db.models.BigAutoField'
--- app_profile/apps.py
+++ app_profile/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class AppProfileConfig(AppConfig):
     name = 'app_profile'
+    default_auto_field = 'django.db.models.BigAutoField'
+    test = 1
